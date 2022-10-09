a05c5cf491f930089b12dc233414a0c15cf51a37
zoutigo <zoutigo@gmail.com>
2022-10-09 22:25:46 +0200

    all dockerized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './modules/common/common.module';
+import { PermissionModule } from './modules/permission/permission.module';
+import { RoleModule } from './modules/role/role.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_URL: Joi.string().required(),
+      }),
+      envFilePath: './env',
+    }),
+    CommonModule,
+    PermissionModule,
+    RoleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
